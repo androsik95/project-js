a8ea412096b1dd1ec6381f7d4d0c5a3f3c0bcbb1
androsik95 <gerasimukandrej@gmail.com>
2023-08-27 19:18:34 +0300

    two commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+let data = [];
+
 const Lists=document.querySelectorAll ('.List')
 const button=document.querySelector('.button')
-
-function addtask() {
-    const btn=document.querySelector('.add__btn')
+const btn=document.querySelector('.add__btn')
     const addbtn=document.querySelector('.add__item-btn')
     const cancelbtn=document.querySelector('.cancel__item-btn')
     const textarea=document.querySelector('.textarea')
     const form=document.querySelector('.form')
+    const titles= document.querySelectorAll('.title')
     let value
+    let draggedItem=null
     
+
     btn.addEventListener('click',()=> { 
         form.style.display='block'
         btn.style.display='none'
@@ -45,75 +48,74 @@ function addtask() {
         dargNdrop() 
 
     }) 
-}
-addtask()
 
-function addBoard () {  
- const boards= document.querySelector('.boards')
- const board= document.createElement('div')
- board.classList.add ('boards__item')
- board.innerHTML=`
- <span contenteditable="true" class="title"> Введите название </span>
- <div class="List"></div>`
- boards.append(board)
- changeTitle ()
- dargNdrop() 
 
-}
 
+const addBoard = function () {
+const boards= document.querySelector('.boards')
+const board= document.createElement('div')
+board.classList.add ('boards__item')
+board.innerHTML=`
+<span contenteditable="true" class="title"> Введите название </span>
+<div class="List"></div>`
+boards.append(board)
+changeTitle ()
+dargNdrop() 
+ 
+};
 button.addEventListener( 'click', addBoard)
 
-function changeTitle () {  
-    const titles= document.querySelectorAll('.title')
-   titles.forEach (title => { 
-    title.addEventListener('click',e=>e.target.textContent='')
-   })
-   }
-   changeTitle ()
+const changeTitle = function () {
+       titles.forEach (title => { 
+        title.addEventListener('click',e=>e.target.textContent='')
+       })
+       }
+       changeTitle ()
+       
+     
 
-   let draggedItem=null
 
-   function dargNdrop() {  
-    const ListItems=document.querySelectorAll ('.List__item')
-    const Lists=document.querySelectorAll ('.List')
 
-    for ( let i=0; i < ListItems.length; i++ ) {
-        const item=ListItems[i]
-        
-        item.addEventListener( 'dragstart', () => { 
-            draggedItem=item
-           setTimeout ( ()=> {
-            item.style.display='none'
-           },0)
-        })
-        item.addEventListener( 'dragend', () => { 
-            setTimeout ( ()=> {
-                item.style.display='block'
-                draggedItem=null
-               },0)
-            
-        })
-        item.addEventListener( 'dblclick', () => { 
-          item.remove()
-        })
-        for ( let j=0; j < Lists.length; j++) {
-            const List=Lists[j]
-            List.addEventListener( 'dragover', e =>e.preventDefault())
-
-            List.addEventListener ( 'dragenter', function (e) {
-                e.preventDefault()
-                this.style.backgroundColor ='rgba (0,0,0,.3)'
-            })
-            List.addEventListener ( 'dragleave', function (e) {
-                this.style.backgroundColor ='rgba (0,0,0, 0)'
-            })
-            List.addEventListener ( 'drop', function (e) {
-                this.style.backgroundColor ='rgba (0,0,0, 0)'
-                this.append(draggedItem)
-            })
-        }
-    }
-
-   }
-   dargNdrop() 
-
+       const dargNdrop = function () {
+        const ListItems=document.querySelectorAll ('.List__item')
+           const Lists=document.querySelectorAll ('.List')
+       
+           for ( let i=0; i < ListItems.length; i++ ) {
+               const item=ListItems[i]
+               
+               item.addEventListener( 'dragstart', () => { 
+                   draggedItem=item
+                  setTimeout ( ()=> {
+                   item.style.display='none'
+                  },0)
+               })
+               item.addEventListener( 'dragend', () => { 
+                   setTimeout ( ()=> {
+                       item.style.display='block'
+                       draggedItem=null
+                      },0)
+                   
+               })
+               item.addEventListener( 'dblclick', () => { 
+                 item.remove()
+               })
+               for ( let j=0; j < Lists.length; j++) {
+                   const List=Lists[j]
+                   List.addEventListener( 'dragover', e =>e.preventDefault())
+       
+                   List.addEventListener ( 'dragenter', function (e) {
+                       e.preventDefault()
+                       this.style.backgroundColor ='rgba (0,0,0,.3)'
+                   })
+                   List.addEventListener ( 'dragleave', function (e) {
+                       this.style.backgroundColor ='rgba (0,0,0, 0)'
+                   })
+                   List.addEventListener ( 'drop', function (e) {
+                       this.style.backgroundColor ='rgba (0,0,0, 0)'
+                       this.append(draggedItem)
+                   })
+               }
+           }
+       
+       
+           }
